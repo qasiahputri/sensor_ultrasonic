47d37f6116138a12471e9e56b88e3f051cd581e9
qasiahputri <73013849+qasiahputri@users.noreply.github.com>
2020-11-01 08:36:49 +0700

    Add files via upload

---
+++ QASI_AH_PTRI_TT3D_ProjectSensorUltraSonic.ino
@@ -0,0 +1,44 @@
+const int pingPin=7;
+const int echoPin=6;
+const int ledPin=13;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  long duration;
+  long cm;
+  pinMode(pingPin,OUTPUT);
+  digitalWrite(pingPin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pingPin,LOW);
+
+  pinMode(echoPin,INPUT);
+  duration=pulseIn(echoPin,HIGH);
+  cm=microsecondsToCentimeters(duration);
+
+  if(cm<=200)
+  {
+    digitalWrite(ledPin,HIGH);
+    
+  }
+
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+
+  delay(100);
+}
+
+  long microsecondsToCentimeters(long microseconds)
+  {
+  return microseconds*0.034/2;
+  }
+
+
